7d2338cf8b23f18f05643b1e4ddde1f779662ae5
samsimsom <samsimsom@gmail.com>
2025-05-23 17:11:08 +0300

    Add PlayerInput script for handling player input actions
    Introduced a `PlayerInput` script to manage movement and look inputs using the Unity Input System. The script is integrated into the player GameObject in the main scene, laying the groundwork for additional input handling like attack, interact, and jump actions in the future.

---
+++ epitaph/Assets/Epitaph/Scripts/Player/Input/PlayerInput.cs
@@ -0,0 +1,101 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Epitaph.Scripts.Player.Input
+{
+    public class PlayerInput : MonoBehaviour, PlayerInputActions.IPlayerActions
+    {
+        public Vector2 MoveInput { get; private set; }
+        public bool IsMoveInput { get; private set; }
+        
+        public Vector2 LookInput { get; private set; }
+        public bool IsLookInput { get; private set; }
+        
+        
+        private PlayerInputActions _playerInputActions;
+
+        private void Awake()
+        {
+            if (_playerInputActions != null) return;
+            _playerInputActions = new PlayerInputActions();
+            _playerInputActions.Player.SetCallbacks(this);
+        }
+
+        private void OnEnable()
+        {
+            _playerInputActions.Player.Enable();
+        }
+
+        private void OnDisable()
+        {
+            _playerInputActions.Player.Disable();
+        }
+        
+        // ---------------------------------------------------------------------------- //
+        
+        public void OnMove(InputAction.CallbackContext context)
+        {
+            if (context.performed)
+            {
+                MoveInput = context.ReadValue<Vector2>();
+                IsMoveInput = MoveInput.magnitude != 0;
+            }
+            else if (context.canceled)
+            {
+                MoveInput = Vector2.zero;
+                IsMoveInput = false;
+            }
+        }
+
+        public void OnLook(InputAction.CallbackContext context)
+        {
+            if (context.performed)
+            {
+                LookInput = context.ReadValue<Vector2>();
+                IsLookInput = LookInput.magnitude != 0;
+            }
+            else if (context.canceled)
+            {
+                LookInput = Vector2.zero;
+                IsLookInput = false;
+            }
+        }
+
+        public void OnAttack(InputAction.CallbackContext context)
+        {
+            
+        }
+
+        public void OnInteract(InputAction.CallbackContext context)
+        {
+            
+        }
+
+        public void OnCrouch(InputAction.CallbackContext context)
+        {
+            
+        }
+
+        public void OnJump(InputAction.CallbackContext context)
+        {
+            
+        }
+
+        public void OnPrevious(InputAction.CallbackContext context)
+        {
+            
+        }
+
+        public void OnNext(InputAction.CallbackContext context)
+        {
+            
+        }
+
+        public void OnSprint(InputAction.CallbackContext context)
+        {
+            
+        }
+        
+        // ---------------------------------------------------------------------------- //
+    }
+}
