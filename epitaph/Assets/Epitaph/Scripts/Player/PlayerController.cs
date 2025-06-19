8a87c5aab9b74dc9fcacfa25b3196d2acac2e238
samsimsom <samsimsom@gmail.com>
2025-06-19 17:38:20 +0300

    update PlayerController to include Cinemachine integration

--- epitaph/Assets/Epitaph/Scripts/Player/PlayerController.cs
+++ epitaph/Assets/Epitaph/Scripts/Player/PlayerController.cs
@@ -1,3 +1,4 @@
+using Unity.Cinemachine;
 using UnityEngine;
 
 namespace Epitaph.Scripts.Player
@@ -7,5 +8,28 @@ namespace Epitaph.Scripts.Player
         [SerializeField] private CharacterController characterController;
         [SerializeField] private Camera playerCamera;
         [SerializeField] private Transform cameraTransform;
+
+        public CharacterController CharacterController => characterController;
+        public Camera PlayerCamera => playerCamera;
+        public CinemachineCamera FpCamera
+        {
+            get
+            {
+                var cinemachineBrain = playerCamera.GetComponent<CinemachineBrain>();
+                return cinemachineBrain.ActiveVirtualCamera as CinemachineCamera;
+            }
+        }
+        public Transform CameraTransform => cameraTransform;
+        
+        
+        // ---------------------------------------------------------------------------- //
+
+        private void Start()
+        {
+            Debug.Log(FpCamera.name);
+        }
+        
+        // ---------------------------------------------------------------------------- //
+        
     }
 }
